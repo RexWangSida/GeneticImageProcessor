ed18e65bd72cb12a8e489f93c34477510cbf7b8c
Bryan Chang <Bryanchang1999@gmail.com>
2020-05-01 19:51:08 -0400

    update access routine for evolve.py

--- evolve.py
+++ evolve.py
@@ -1,1 +1,17 @@
+## @file evolve.py
+#  @author Bryan Chang
+#  @date May 1st 2020
 
+#  @brief This file contains the function which is the skeleton of the genertic
+#  algorithm
+
+## @brief This function will generate population with size N and mutate for X
+#  generations with mutate rate of Z and output the final result
+#  @param imageIn PPM file, the target image for the evolution
+#  @param numGenerations int, the number of generation for the evolution
+#  @param populationSize int, the total number of the population
+#  @param mutateRate float, the percent of pixel that will be mutated for the image
+#  @return imageOut the image that is most similiar to imageIn
+def evolve_image(imageIn, numGenerations, populationSize, mutateRate):
+    imageOut = imageIn
+    return imageOut
