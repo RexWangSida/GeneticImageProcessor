effdf44006d07061e54a8cefcbdc894a7ff7f19d
RexWangSida <wangs132@mcmaster.ca>
2020-05-01 00:45:47 -0400

    added Doxygen Comment for easier team work

--- Individual.py
+++ Individual.py
@@ -1,4 +1,12 @@
+## @file Individual.py
+#  @author Sida Wang
+#  @date May 1st 2020
+
+#  @brief This a template module that gives a abstract representation of an individual image, with its difference to the given image.
 class Individual(object):
+    ## @brief This is constructor of the Individual template.
+    #  @param image The PPM image that it represents.
+    #  @param fitness A value that shows how similar it is compared to the given image. The better fitness it has, the more similar it is compared to the given image.
     def _init_(image, fitness):
         self.image = image ##image is an instance of PPMImage
         self.fitness = fitness ##fitness is the difference between the given image and the new generated image
--- PPMImage.py
+++ PPMImage.py
@@ -1,4 +1,14 @@
+## @file PPMImage.py
+#  @author Sida Wang
+#  @date May 1st 2020
+
+#  @brief This a template module that gives a template of an PPM Image for processing.
 class PPMImage(object):
+    ## @brief This is constructor of the PPM Image template.
+    #  @param pixel A list of dictionaries, each dictionary represents an individual pixel.
+    #  @param width The width of the PPM Image.
+    #  @param height The height of the PPM Image.
+    #  @param maxColor The maximum number of the r,g,b of the pixels in the image.
     def _init_(pixel, width, height, maxColor):
         self.pixel = pixel ##pixel is a list of dictionaries with r,g,b
         self.width = width
