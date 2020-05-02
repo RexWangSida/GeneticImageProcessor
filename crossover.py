b6d3cc3fe19ad056acf960238b9a17f83503bc18
Bryan Chang <changb9@mcmaster.ca>
2020-05-02 16:20:57 -0400

    modify crossover fit for dictionary

--- crossover.py
+++ crossover.py
@@ -1,7 +1,6 @@
 ## @file crossover.py
 #  @author Bryan Chang
 #  @date May 1st, 2020
-import Individual.py
 import random
 
 #  @brief This is the file responsible for generating new children through
@@ -13,41 +12,22 @@ import random
 #  @param child1 Individual as first child that will be generated based on two parents
 #  @param child2 Individual as second child that will be generated based on two parents
 def recombine(parent1, parent2, child1, child2):
-	parent1Image = parent1.getImage()
-	parent2Image = parent2.getImage()
-	child1Image = child1.getImage()
-	child2Image = child2.getImage()
-	width, height = parent1.getImage().size
-	crossoverwidth = random.randint(0, width)
-	crossoverheight = random.randint(0, height)
+	totalPixel = len(parent1["image"]["pixels"])
+	crossoverPoint = random.randint(0,totalPixel)
 
-	## The following two for loops will copy parent1,2 to child1,2 from [0,0] to
-	## [crossoverwidth, crossoverheight]
-	for i in range(0,crossoverheight - 1):
-		for j in range(0, width):
-			child1Image[j,i] = parent1Image[j,i]
-			child2Image[j,i] = parent2Image[j,i]
+	## The following for loop will copy parent1,2 to child1,2 from [0] to
+	## [crossoverPoint]
+	for i in range(0, crossoverPoint):
+		child1["image"]["pixels"][i] = parent1["image"]["pixels"][i]
+		child2["image"]["pixels"][i] = parent2["image"]["pixels"][i]
+	## The following for loop will copy parent1,2 to child2,1 from
+	## [crossoverPoint] to [totalPixel - 1]
+	for i in range(crossoverPoint, totalPixel):
+		child2["image"]["pixels"][i] = parent1["image"]["pixels"][i]
+		child1["image"]["pixels"][i] = parent2["image"]["pixels"][i]
 
-	for i in range(0, crossoverwidth):
-			child1Image[i,crossoverheight] = parent1Image[i,crossoverheight]
-			child2Image[i,crossoverheight] = parent2Image[i,crossoverheight]
 
-	## The following two for loops will copy parent1,2 to child2,1 from
-	## [crossoverwidth, crossoverheight] to [width - 1, height - 1]
-	for i in range(crossoverwidth, width):
-		child1Image[i,crossoverheight] = parent2Image[i,crossoverheight]
-		child2Image[i,crossoverheight] = parent1Image[i,crossoverheight]
-
-	for i in range(crossoverheight + 1,height):
-		for j in range(0, width):
-			child1Image[j,i] = parent1Image[j,i]
-			child2Image[j,i] = parent2Image[j,i]
-
-	##upload new gnerated image to child1 and child 2
-	child1.changeImage(child1Image)
-	child2.changeImage(child2Image)
-
-	return none
+	return None
 
 ## @brief This function will generate the second half of the population based
 #  on the first half of the population
