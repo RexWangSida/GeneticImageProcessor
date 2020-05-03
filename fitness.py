f1cdc3c7572ae50a955b607e69cf9eaa31f9751a
Deuce-Cao <hq.cao1@gmail.com>
2020-05-03 00:18:11 -0400

    modify class structure

--- fitness.py
+++ fitness.py
@@ -5,56 +5,56 @@
 #  @brief This file contains the methods of calculationg fitness
 
 from math import sqrt
+class fitness():
+	## @brief This method calculate the sum of RGB color distance pixel by pixel between two images
+	#  @param A, the pixels of 1st image
+	#  @param B, the pixels of 2nd image
+	#  @param imageSize, the size of images
+	#  @return distance, the distance of two images
+	def __compDistance(A, B, imageSize):
+	    sum = 0
+	    reminder = imageSize %2
+	    if(reminder == 0):
+	        for i in range(0, imageSize, 2):
+	            r = A[i]['r'] - B[i]['r']
+	            g = A[i]['g'] - B[i]['g']
+	            b = A[i]['b'] - B[i]['b']
+	            sum += r*r + g*g + b*b
+	            r = A[i+1]['r'] - B[i+1]['r']
+	            g = A[i+1]['g'] - B[i+1]['g']
+	            b = A[i+1]['b'] - B[i+1]['b']
+	            sum += r*r + g*g + b*b
+	    elif(imageSize == 1):
+	        r = A[-1]['r'] - B[-1]['r']
+	        g = A[-1]['g'] - B[-1]['g']
+	        b = A[-1]['b'] - B[-1]['b']
+	        sum += r*r + g*g + b*b
+	    elif(reminder == 1):
+	        for i in range(0, imageSize, 2):
+	            r = A[i]['r'] - B[i]['r']
+	            g = A[i]['g'] - B[i]['g']
+	            b = A[i]['b'] - B[i]['b']
+	            sum += r*r + g*g + b*b
+	            r = A[i+1]['r'] - B[i+1]['r']
+	            g = A[i+1]['g'] - B[i+1]['g']
+	            b = A[i+1]['b'] - B[i+1]['b']
+	            sum += r*r + g*g + b*b
 
-## @brief This method calculate the sum of RGB color distance pixel by pixel between two images
-#  @param A, the pixels of 1st image
-#  @param B, the pixels of 2nd image
-#  @param image_size, the size of images
-#  @return distance, the distance of two images
-def comp_distance(A, B, image_size):
-    sum = 0
-    reminder = image_size %2
-    if(reminder == 0):
-        for i in range(0, image_size, 2):
-            r = A[i]['r'] - B[i]['r']
-            g = A[i]['g'] - B[i]['g']
-            b = A[i]['b'] - B[i]['b']
-            sum += r*r + g*g + b*b
-            r = A[i+1]['r'] - B[i+1]['r']
-            g = A[i+1]['g'] - B[i+1]['g']
-            b = A[i+1]['b'] - B[i+1]['b']
-            sum += r*r + g*g + b*b
-    elif(image_size == 1):
-        r = A[-1]['r'] - B[-1]['r']
-        g = A[-1]['g'] - B[-1]['g']
-        b = A[-1]['b'] - B[-1]['b']
-        sum += r*r + g*g + b*b
-    elif(reminder == 1):
-        for i in range(0, image_size, 2):
-            r = A[i]['r'] - B[i]['r']
-            g = A[i]['g'] - B[i]['g']
-            b = A[i]['b'] - B[i]['b']
-            sum += r*r + g*g + b*b
-            r = A[i+1]['r'] - B[i+1]['r']
-            g = A[i+1]['g'] - B[i+1]['g']
-            b = A[i+1]['b'] - B[i+1]['b']
-            sum += r*r + g*g + b*b
+	        r = A[-1]['r'] - B[-1]['r']
+	        g = A[-1]['g'] - B[-1]['g']
+	        b = A[-1]['b'] - B[-1]['b']
+	        sum += r*r + g*g + b*b
 
-        r = A[-1]['r'] - B[-1]['r']
-        g = A[-1]['g'] - B[-1]['g']
-        b = A[-1]['b'] - B[-1]['b']
-        sum += r*r + g*g + b*b
+	    distance = sqrt(sum)
+	    return distance
 
-    distance = sqrt(sum)
-    return distance
+	## @brief This method calculate fitness between each individual in a population and the input image
+	#  @param image, the pixels of input image
+	#  @param population, the population of all individuals
+	#  @param populationSize, the size of population
+	#  @detail save the fitness to each individual
+	def compFitnessPopulation(image, population, populationSize):
+	    size = population[0]['image']['width'] * population[0]['image']['height']
 
-## @brief This method calculate fitness between each individual in a population and the input image
-#  @param image, the pixels of input image
-#  @param population, the population of all individuals
-#  @param population_size, the size of population
-#  @detail save the fitness to each individual
-def comp_fitness_population(image, population, population_size):
-    size = population[0]['image']['width'] * population[0]['image']['height']
-
-    for i in range(0, population_size):
-        population[i]['fitness'] = comp_distance(population[i]['image']['pixels'], image, size)
+	    for i in range(0, populationSize):
+	        population[i]['fitness'] = compDistance(population[i]['image']['pixels'], image, size)
--- population.py
+++ population.py
@@ -5,36 +5,36 @@
 #  @brief This file contains the methods of generationg random images
 
 from random import randint
+class population():
+	## @brief This method generates image with certificated size and randomized color each pixel
+	#  @param width, the width of image
+	#  @param height, the height of image
+	#  @param maxColor, the maximum value of each RGB color
+	#  @return L, a list of all pixels as dictionary with RGB color
+	def __generateRandomImage(width, height, maxColor):
+	    randomSize = height * width
 
-## @brief This method generates image with certificated size and randomized color each pixel
-#  @param width, the width of image
-#  @param height, the height of image
-#  @param max_color, the maximum value of each RGB color
-#  @return L, a list of all pixels as dictionary with RGB color
-def generate_random_image(width, height, max_color):
-    random_size = height * width
+	    pixels = []
 
-    pixels = []
+	    for i in range(0, randomSize):
+	        pixels.append({'r':randint(0, maxColor),
+	                       'g':randint(0, maxColor),
+	                       'b':randint(0, maxColor)})
+	    return pixels;
 
-    for i in range(0, random_size):
-        pixels.append({'r':randint(0, max_color),
-                       'g':randint(0, max_color),
-                       'b':randint(0, max_color)})
-    return pixels;
+	## @brief This method generates a population with certificated size and contains multiple image
+	#  @param populationSize, the size of population
+	#  @param width, the width of each image
+	#  @param height, the height of each image
+	#  @param maxColor, the maximum value of each RGB color in each image
+	#  @return population, a list of all individuals as dictionary with image
+	def generatePopulation(populationSize, width, height, maxColor):
+	    population = []
 
-## @brief This method generates a population with certificated size and contains multiple image
-#  @param population_size, the size of population
-#  @param width, the width of each image
-#  @param height, the height of each image
-#  @param max_color, the maximum value of each RGB color in each image
-#  @return population, a list of all individuals as dictionary with image
-def generate_population(population_size, width, height, max_color):
-    population = []
-
-    for i in range(0, population_size):
-        population.append({'image':{'width':width,
-                                    'height':height,
-                                    'max_color':max_color,
-                                    'pixels':generate_random_image(width, height, max_color)},
-                           'fitness':100000000})
-    return population;
+	    for i in range(0, populationSize):
+	        population.append({'image':{'width':width,
+	                                    'height':height,
+	                                    'maxColor':maxColor,
+	                                    'pixels':generateRandomImage(width, height, maxColor)},
+	                           'fitness':100000000})
+	    return population;
