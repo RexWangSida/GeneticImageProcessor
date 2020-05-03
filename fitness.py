dd8281e76bcd30f7d0fa1dd8e27cdc0d767c0018
Deuce-Cao <hq.cao1@gmail.com>
2020-05-02 23:13:22 -0400

    modify formats and bug fix for fitness

--- fitness.py
+++ fitness.py
@@ -4,66 +4,49 @@
 
 #  @brief This file contains the methods of calculationg fitness
 
+from math import sqrt
+
 ## @brief This method calculate the sum of RGB color distance pixel by pixel between two images
 #  @param A, the pixels of 1st image
 #  @param B, the pixels of 2nd image
 #  @param image_size, the size of images
 #  @return distance, the distance of two images
 def comp_distance(A, B, image_size):
-	sum = 0
-	reminder = image_size %2
-	last = image_size - 1
-	if(reminder == 0):
-		for i in range(0, image_size, 2):
-			r = A[i]['r'] - B[i].['r']
-            g = A[i]['g'] - B[i].['g']
-            b = A[i]['b'] - B[i].['b']
-			r_2 = r*r
-			g_2 = g*g
-			b_2 = b*b
-			sum += r_2 + g_2 + b_2
-			r = A[i+1]['r'] - B[i+1]['r']
-			g = A[i+1]['g'] - B[i+1]['g']
-			b = A[i+1]['b'] - B[i+1]['b']
-			r_2 = r*r
-			g_2 = g*g
-			b_2 = b*b
-			sum += r_2 + g_2 + b_2
-	elif(image_size == 1):
-		r = A[last]['r'] - B[last]['r']
-		g = A[last]['g'] - B[last]['g']
-		b = A[last]['b'] - B[last]['b']
-		r_2 = r*r
-		g_2 = g*g
-		b_2 = b*b
-		sum += r_2 + g_2 + b_2
-	elif(reminder == 1):
-		for i in range(0, image_size, 2):
-			r = A[i]['r'] - B[i]['r']
-			g = A[i]['g'] - B[i]['g']
-			b = A[i]['b'] - B[i]['b']
-			r_2 = r*r
-			g_2 = g*g
-			b_2 = b*b
-			sum += r_2 + g_2 + b_2
-			r = A[i+1]['r'] - B[i+1]['r']
-			g = A[i+1]['g'] - B[i+1]['g']
-			b = A[i+1]['b'] - B[i+1]['b']
-			r_2 = r*r
-			g_2 = g*g
-			b_2 = b*b
-			sum += r_2 + g_2 + b_2
+    sum = 0
+    reminder = image_size %2
+    if(reminder == 0):
+        for i in range(0, image_size, 2):
+            r = A[i]['r'] - B[i]['r']
+            g = A[i]['g'] - B[i]['g']
+            b = A[i]['b'] - B[i]['b']
+            sum += r*r + g*g + b*b
+            r = A[i+1]['r'] - B[i+1]['r']
+            g = A[i+1]['g'] - B[i+1]['g']
+            b = A[i+1]['b'] - B[i+1]['b']
+            sum += r*r + g*g + b*b
+    elif(image_size == 1):
+        r = A[-1]['r'] - B[-1]['r']
+        g = A[-1]['g'] - B[-1]['g']
+        b = A[-1]['b'] - B[-1]['b']
+        sum += r*r + g*g + b*b
+    elif(reminder == 1):
+        for i in range(0, image_size, 2):
+            r = A[i]['r'] - B[i]['r']
+            g = A[i]['g'] - B[i]['g']
+            b = A[i]['b'] - B[i]['b']
+            sum += r*r + g*g + b*b
+            r = A[i+1]['r'] - B[i+1]['r']
+            g = A[i+1]['g'] - B[i+1]['g']
+            b = A[i+1]['b'] - B[i+1]['b']
+            sum += r*r + g*g + b*b
 
-		r = A[last]['r'] - B[last]['r']
-		g = A[last]['g'] - B[last]['g']
-		b = A[last]['b'] - B[last]['b']
-		r_2 = r*r
-		g_2 = g*g
-		b_2 = b*b
-		sum += r_2 + g_2 + b_2
+        r = A[-1]['r'] - B[-1]['r']
+        g = A[-1]['g'] - B[-1]['g']
+        b = A[-1]['b'] - B[-1]['b']
+        sum += r*r + g*g + b*b
 
-	distance = sqrt(sum)
-	return distance
+    distance = sqrt(sum)
+    return distance
 
 ## @brief This method calculate fitness between each individual in a population and the input image
 #  @param image, the pixels of input image
@@ -71,7 +54,7 @@ def comp_distance(A, B, image_size):
 #  @param population_size, the size of population
 #  @detail save the fitness to each individual
 def comp_fitness_population(image, population, population_size):
-	size = population[0]['image']['width'] * population[0]['image']['height']
+    size = population[0]['image']['width'] * population[0]['image']['height']
 
-	for i in range(0, population_size):
-		population[i]['fitness'] = comp_distance(population[i]['image']['data'], image, size)
+    for i in range(0, population_size):
+        population[i]['fitness'] = comp_distance(population[i]['image']['pixels'], image, size)
