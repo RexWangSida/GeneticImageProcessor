2b500c721b56494f8dad651349ff2e8c2e612cb8
RexWangSida <wangs132@mcmaster.ca>
2020-05-06 09:37:52 -0400

    integration finished

--- population.py
+++ population.py
@@ -55,67 +55,67 @@ class population:
                             'b': randint(0,maxColor)})
         return pixels
 
-	def __compFitnessPopulation(self, image, populationSize):
-		size = self.population[0]['image']['width'] * self.population[0]['image']['height']
+    def __compFitnessPopulation(self, image, populationSize):
+    size = self.population[0]['image']['width'] * self.population[0]['image']['height']
 
-		for i in range(0, populationSize):
-			self.population[i]['fitness'] = self.__compDistance(self.population[i]['image']['pixels'], image, size)
+    for i in range(0, populationSize):
+    self.population[i]['fitness'] = self.__compDistance(self.population[i]['image']['pixels'], image, size)
 
-	def __compDistance(self, A, B, imageSize):
-		sum = 0
-		reminder = imageSize %2
-		if(reminder == 0):
-			for i in range(0, imageSize, 2):
-				r = A[i]['r'] - B[i]['r']
-				g = A[i]['g'] - B[i]['g']
-				b = A[i]['b'] - B[i]['b']
-				sum += r*r + g*g + b*b
-				r = A[i+1]['r'] - B[i+1]['r']
-				g = A[i+1]['g'] - B[i+1]['g']
-				b = A[i+1]['b'] - B[i+1]['b']
-				sum += r*r + g*g + b*b
-		elif(imageSize == 1):
-			r = A[-1]['r'] - B[-1]['r']
-			g = A[-1]['g'] - B[-1]['g']
-			b = A[-1]['b'] - B[-1]['b']
-			sum += r*r + g*g + b*b
-		elif(reminder == 1):
-			for i in range(0, imageSize, 2):
-				r = A[i]['r'] - B[i]['r']
-				g = A[i]['g'] - B[i]['g']
-				b = A[i]['b'] - B[i]['b']
-				sum += r*r + g*g + b*b
-				r = A[i+1]['r'] - B[i+1]['r']
-				g = A[i+1]['g'] - B[i+1]['g']
-				b = A[i+1]['b'] - B[i+1]['b']
-				sum += r*r + g*g + b*b
+    def __compDistance(self, A, B, imageSize):
+    sum = 0
+    reminder = imageSize %2
+    if(reminder == 0):
+    for i in range(0, imageSize, 2):
+    r = A[i]['r'] - B[i]['r']
+    g = A[i]['g'] - B[i]['g']
+    b = A[i]['b'] - B[i]['b']
+    sum += r*r + g*g + b*b
+    r = A[i+1]['r'] - B[i+1]['r']
+    g = A[i+1]['g'] - B[i+1]['g']
+    b = A[i+1]['b'] - B[i+1]['b']
+    sum += r*r + g*g + b*b
+    elif(imageSize == 1):
+    r = A[-1]['r'] - B[-1]['r']
+    g = A[-1]['g'] - B[-1]['g']
+    b = A[-1]['b'] - B[-1]['b']
+    sum += r*r + g*g + b*b
+    elif(reminder == 1):
+    for i in range(0, imageSize, 2):
+    r = A[i]['r'] - B[i]['r']
+    g = A[i]['g'] - B[i]['g']
+    b = A[i]['b'] - B[i]['b']
+    sum += r*r + g*g + b*b
+    r = A[i+1]['r'] - B[i+1]['r']
+    g = A[i+1]['g'] - B[i+1]['g']
+    b = A[i+1]['b'] - B[i+1]['b']
+    sum += r*r + g*g + b*b
 
-			r = A[-1]['r'] - B[-1]['r']
-			g = A[-1]['g'] - B[-1]['g']
-			b = A[-1]['b'] - B[-1]['b']
-			sum += r*r + g*g + b*b
+    r = A[-1]['r'] - B[-1]['r']
+    g = A[-1]['g'] - B[-1]['g']
+    b = A[-1]['b'] - B[-1]['b']
+    sum += r*r + g*g + b*b
 
-		distance = sqrt(sum)
-		return distance
+    distance = sqrt(sum)
+    return distance
 
-	def __crossover(self, populationSize):
+    def __crossover(self, populationSize):
 
-		for i in range(0, populationSize // 2, 2):
-			## parent i and i+1 produce children population_size/2+i and
-			## population_size/2+i+1.
-			totalPixel = len(self.population[i]["image"]["pixels"])
-			crossoverPoint = random.randint(0,totalPixel)
+    for i in range(0, populationSize // 2, 2):
+    ## parent i and i+1 produce children population_size/2+i and
+    ## population_size/2+i+1.
+    totalPixel = len(self.population[i]["image"]["pixels"])
+    crossoverPoint = random.randint(0,totalPixel)
 
-			## The following for loop will copy parent1,2 to child1,2 from [0] to
-			## [crossoverPoint]
-			for i in range(0, crossoverPoint):
-				self.population[i + populationSize // 2]["image"]["pixels"][i] = self.population[i]["image"]["pixels"][i]
-				self.population[i + populationSize // 2 + 1]["image"]["pixels"][i] = self.population[i + 1]["image"]["pixels"][i]
-			## The following for loop will copy parent1,2 to child2,1 from
-			## [crossoverPoint] to [totalPixel - 1]
-			for i in range(crossoverPoint, totalPixel):
-				self.population[i + populationSize // 2 + 1]["image"]["pixels"][i] = self.population[i]["image"]["pixels"][i]
-				self.population[i + populationSize // 2]["image"]["pixels"][i] = self.population[i + 1]["image"]["pixels"][i]
+    ## The following for loop will copy parent1,2 to child1,2 from [0] to
+    ## [crossoverPoint]
+    for i in range(0, crossoverPoint):
+    self.population[i + populationSize // 2]["image"]["pixels"][i] = self.population[i]["image"]["pixels"][i]
+    self.population[i + populationSize // 2 + 1]["image"]["pixels"][i] = self.population[i + 1]["image"]["pixels"][i]
+    ## The following for loop will copy parent1,2 to child2,1 from
+    ## [crossoverPoint] to [totalPixel - 1]
+    for i in range(crossoverPoint, totalPixel):
+    self.population[i + populationSize // 2 + 1]["image"]["pixels"][i] = self.population[i]["image"]["pixels"][i]
+    self.population[i + populationSize // 2]["image"]["pixels"][i] = self.population[i + 1]["image"]["pixels"][i]
 
     def __mutate(Individual, rate):
         imageSize = Individual['image']['width'] * Individual['image']['height']
